f4c48e12399d5f117a19e1a3d0fcbafecceed8ec
Arihant Verma <arihantverma1994@gmail.com>
2024-07-18 19:54:24 +0530

    feat: 404, mobile nav ⍙, trigger warning dialog (#6)
    - Custom 404 page
    - tailwind breakpoint book
    - mobile navigation menu: open only 90% instead of full width
    - trigger warning dialog.
    - mobile the/tool sidebar UI + state behaviour.

---
+++ src/components/react/hooks/use-breakpoint.ts
@@ -0,0 +1,11 @@
+import { useMediaQuery } from "react-responsive";
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '@root/tailwind.config.mjs'
+
+const config = resolveConfig(tailwindConfig)
+const breakpoints = config.theme.screens
+
+export function useBreakPoint(breakpoint: keyof typeof breakpoints) {
+  const breakpointQuery = breakpoints[breakpoint]
+  return useMediaQuery({ query: `(min-width: ${breakpointQuery})` })
+}
